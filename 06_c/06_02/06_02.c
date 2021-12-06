1c9217dc6603c03cbc6e29179a65fe34891120aa
LukeRabauke <lukas.wizemann@web.de>
2021-12-06 10:37:58 +0100

    Update 06_02.c

--- 06_c/06_02/06_02.c
+++ 06_c/06_02/06_02.c
@@ -63,25 +63,34 @@ int main(int argc, char *argv[])
     
     long long glow_new_array[9]={anzahl_0, anzahl_1, anzahl_2, anzahl_3, anzahl_4, anzahl_5, anzahl_6, anzahl_7, anzahl_8};
     int first_run=0;
+    long long *ptr_array[9]={0};
 
     for (day = 0; day < 80; day++)
     {
-        for(i=7 ;i>=0 ;i--)
+        for(i=0 ;i<9 ;i++)
         { 
-            if (i!=0)
+            if (i==8)
             {
-                if (glow_new_array[i+1]!=0) glow_new_array[i]=glow_new_array[i+1];
-                
-                
+                ptr_array[i]= &glow_new_array[0];
             }
-            else if (i==0)
+            else if (i==6)
             {
-                glow_new_array[8]=glow_new_array[i];
-                glow_new_array[6]+=glow_new_array[i];
+                ptr_array[i]= &glow_new_array[0]+glow_new_array[7];
+            }
+            else if (i!=8 && i!=6)
+            {
+                ptr_array[i]= &glow_new_array[i+1];
             }
             else printf("Error, unzulässige Nummer als Eintrag gefunden!");
             
         }
+
+        for ( i = 0; i < 9; i++)
+        {
+            glow_new_array[i]= *ptr_array[i];
+        }
+        
+
         //current_entries=sliding_numb+1; //übertrage die neuen Tage als neues Schleifenende
         //printf("We are at day: %d \n", day);
     }
