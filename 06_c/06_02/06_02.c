69fb62a50435e519c93978586a3a101d795fef39
LukeRabauke <lukas.wizemann@web.de>
2021-12-06 17:06:03 +0100

    Update 06_02.c

--- 06_c/06_02/06_02.c
+++ 06_c/06_02/06_02.c
@@ -9,17 +9,18 @@ int main(int argc, char *argv[])
     char line[256], *ptr;
     char delim[] = ",";
     int input_vec[1000]={0}, input_counter=0, first_line=0;
-    char *glow_fish = (char*)malloc(9000000000000 * sizeof(char));
+    char glow_fish[1000]={0} ;
     //char glow_fish[7000000]={9};
     int day=0;
-    long long current_entries=300; 
-    long long sliding_numb=299, i=0;
+    long long current_entries=300,i=0;
     int max_old=0;
     long long sum=0;
+    int first_run=0;
+    long long ptr_array[9]={0};
 
     while (fgets(line, sizeof(line), file))
     {
-        printf("%s", line);
+        //printf("%s", line);
 
         if (first_line == 0 && line[0] != '\n')
         {
@@ -48,7 +49,7 @@ int main(int argc, char *argv[])
         }
     }
     fclose(file);
-    printf(" Anzahl an Einträgen in Vektor : %d \n", input_counter);
+    //printf(" Anzahl an Einträgen in Vektor : %d \n", input_counter);
     long long anzahl_0=0, anzahl_1=0, anzahl_2=0, anzahl_3=0, anzahl_4=0, anzahl_5=0, anzahl_6=0, anzahl_7=0, anzahl_8=0;
     for ( i = 0; i < current_entries; i++)
     {
@@ -60,26 +61,23 @@ int main(int argc, char *argv[])
         else if (glow_fish[i]==5) anzahl_5++;
         
     }
-    
     long long glow_new_array[9]={anzahl_0, anzahl_1, anzahl_2, anzahl_3, anzahl_4, anzahl_5, anzahl_6, anzahl_7, anzahl_8};
-    int first_run=0;
-    long long *ptr_array[9]={0};
 
-    for (day = 0; day < 80; day++)
+    for (day = 0; day < 256; day++)
     {
         for(i=0 ;i<9 ;i++)
         { 
             if (i==8)
             {
-                ptr_array[i]= &glow_new_array[0];
+                ptr_array[i] = glow_new_array[0];
             }
             else if (i==6)
             {
-                ptr_array[i]= &glow_new_array[0]+glow_new_array[7];
+                ptr_array[i] = glow_new_array[0]+glow_new_array[7];
             }
             else if (i!=8 && i!=6)
             {
-                ptr_array[i]= &glow_new_array[i+1];
+                ptr_array[i]= glow_new_array[i+1];
             }
             else printf("Error, unzulässige Nummer als Eintrag gefunden!");
             
@@ -87,7 +85,7 @@ int main(int argc, char *argv[])
 
         for ( i = 0; i < 9; i++)
         {
-            glow_new_array[i]= *ptr_array[i];
+            glow_new_array[i] = ptr_array[i];
         }
         
 
